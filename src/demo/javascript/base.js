dc87c1950b7a7abf8b1b4e241846dd17711f1617
GaoFei <strivek@163.com>
2014-12-06 16:50:08 +0800

    封装渲染逻辑

--- src/demo/javascript/base.js
+++ src/demo/javascript/base.js
@@ -1,13 +1,34 @@
-define(['echarts','jquery'], function (ec,$) {
+define(['jquery'], function ($) {
 
     function baseOption(options) {
-        this.chartList = ['a', 'b', 'c', 'd'];
-        this.sqlList = options.sqlList;
+
     }
+
     baseOption.fn = baseOption.prototype;
 
-    baseOption.fn.renderChart = function () {
+    baseOption.fn.renderChart = function (sql, myChart, successMethod/*可选成功方法*/, failMethod/*可选失败方法*/) {
+        var request = $.ajax({
+                url: sql,
+                type: "get",
+                cache: false
+            }),
+            success = successMethod || _success,
+            fail = failMethod || _fail;
 
+        request.done(success);
+        request.fail(fail);
+        //默认成功方法
+        function _success(data) {
+            var chartData = eval(data);
+            myChart.setTheme("dark");
+            myChart.setOption(chartData);
+        }
+        //默认失败
+        function _fail(data) {
+            alert('系统小哥正在开小差，请重新登录');
+        }
     }
+
+    return new baseOption();
 
 })
